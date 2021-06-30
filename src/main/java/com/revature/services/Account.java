ddd6e1ec145e7dc10a2c38e640ca1d4bf3e63f7c
June Greenup <cgreenup2000@gmail.com>
2021-06-30 16:42:47 -0500

    implemented changeBalance method in Account.java

--- src/main/java/com/revature/services/Account.java
+++ src/main/java/com/revature/services/Account.java
@@ -1,11 +1,15 @@
 package com.revature.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Account {
 
 	//Class fields
 	private String name;
 	private String accountType;
 	private double balance;
+	Logger log = LoggerFactory.getLogger(Account.class);
 	
 	//Getters
 	public String getName() 			{ return name; }
@@ -24,9 +28,17 @@ public class Account {
 	//Changes the balance of the account
 	//Positive change signifies a deposit
 	//Negative change signifies a withdrawal
-	public void changeBalance(double change) { 
+	public void changeBalance(double change) throws NegativeAccountException { 
 		double temp = roundToTwo(balance) + roundToTwo(change);
+		
+		if (temp < 0.00) {
+			log.error("Account attempted a negative balance.");
+			log.error(roundToTwo(balance) + " + " + roundToTwo(change) + " = " + temp);
+			throw new NegativeAccountException("Account attempted a negative balance.");
 		}
+		
+		this.balance = temp;
+	}
 	
 	public static double roundToTwo(double input) {
 		return (double) (Math.round(input * 100.0) / 100.0);
