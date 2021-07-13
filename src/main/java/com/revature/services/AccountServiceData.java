a2df65f075d7512534d7e64fd73dea9b62179eeb
June Greenup <cgreenup2000@gmail.com>
2021-07-12 22:53:19 -0500

    Added deposit and withdraw to menus

--- src/main/java/com/revature/services/AccountServiceData.java
+++ src/main/java/com/revature/services/AccountServiceData.java
@@ -53,16 +53,94 @@ public class AccountServiceData {
 	
 	
 	//Deposit
+	//Returns the new balance after depositing the funds
+	//Returns -1 if an error occurred
+	public static double depositFunds(Account account, double funds) {
+		try {
+			account.changeBalance(funds);
+			aImplement.updateAccount(account, account.getId());
+			log.info(account.getOwner() + " successfully desposited funds to account");
+			return account.getBalance();
+		} catch (NegativeAccountException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+		log.error("Could not fulfill " + account.getOwner() + "'s deposit request");
+		}
+		return -1;
+	}
+	
 	//Withdraw
+	//Returns the new balance after depositing the funds
+	//Returns -1 if an error occurred
+	public static double withdrawFunds(Account account, double funds) {
+		try {
+			account.changeBalance(-funds);
+			aImplement.updateAccount(account, account.getId());
+			log.info(account.getOwner() + " successfully withdrew funds from account");
+			return account.getBalance();
+		} catch (NegativeAccountException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		finally {
+			log.error("Could not fulfill " + account.getOwner() + "'s withdraw request");
+			}
+		return -1;
+	}
 	
 	//Transfer Funds
 	//Deposit + withdraw
+	public static boolean transferFunds(Account from, Account to, double funds) {
+		double result;
+		log.info("Withdrawing $" + funds + " from " + from.toString());
+		result = withdrawFunds(from, funds);
+		if(result == -1){
+			log.error("Tranfer Failed");
+			return false;
+		}
+		log.info("Depositing $" + funds + " from " + from.toString());
+		result = depositFunds(to, funds);
+		if(result == -1) {
+			log.error("Transfer Failed");
+			return false;
+		}
+		
+		return true;
+		
+	}
 
 	//Approve deny applications
-	//
+	public static boolean acceptApplication(Account account, Boolean isAccepted) {
+		if(aImplement.findId(account) != 0) {
+			log.debug("Account is valid");
+			if(isAccepted) {
+				account.setIsApplication(isAccepted);
+				aImplement.updateAccount(account, account.getId());
+				log.info("Application for account approved");
+			}else {
+				aImplement.removeAccount(account);
+			}
+			
+		}
+		
+		return false;
+	}
 	
 	
-	
+	//Delete Account
+	public static boolean removeAccount(Account account) {
+		log.info("Removing Account: " + account.toString());
+		if(aImplement.findId(account) != 0) {
+			aImplement.removeAccount(account);
+			log.info("Account Removed successful");
+			return true;
+		}
+		
+		log.info("Account removal failed: no such account exists in the database");
+		return false;
+	}
 	
 	
 	//debug
--- src/main/java/com/revature/ui/AccountMenu.java
+++ src/main/java/com/revature/ui/AccountMenu.java
@@ -1,5 +1,7 @@
 package com.revature.ui;
 
+import java.awt.font.NumericShaper;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,27 +14,91 @@ public class AccountMenu {
 	
 	public void manageAccounts(Profile profile) {
 		AccountServiceData.getServiceData();
-		final int spacing = 5;
-		System.out.println("Here are all your accounts");
-		System.out.printf("%-"+spacing+"s %-"+ spacing*2 +"s %-" + spacing*3 +"s %s \n",
-				"#", "Balance" , "Account Type", "Account Name" );
-		System.out.println("====================================================");
+		List<Account> connectedAccounts = printAccounts(profile);
+		Account managedAccount;
 		
-		List<Account> userAccounts = AccountServiceData.getConnectedAccounts(profile);
+		if(connectedAccounts.size() == 0)
+			return;
 		
-		for(int i = 0; i < userAccounts.size(); i++){
-			Account acc = userAccounts.get(i);
-			System.out.printf("%-"+spacing+"s %-"+ spacing*2 +"s %-" + spacing*3 +"s %s \n", (i+1) + ")", acc.getBalance(), acc.getAccountType(), acc.getAccountName());
-		}
-		
-		if(userAccounts.size() <= 0) {
-			System.out.println("Sorry, we could not find any accounts under your details.\n"
-							+ "You can always apply for a bank account, and any approved bank accounts will appear here.");
-		}
-		
+		System.out.println("Select the number of the account you would like to manage, or cancel to quit.");
 		System.out.println();
+		String userInput;
+		int userInt;
+		do {
+			userInput = scanner.nextLine().strip();
+			if (!"cancel quit leave exit".contains(userInput)) {
+				try {
+					userInt = Integer.parseInt(userInput);
+					
+					if (userInt > connectedAccounts.size() || userInt < 1) {
+						throw new NumberFormatException();
+					}
+					
+					//Input validation complete
+					//The user does not want to leave
+					//And the user selected an account within range
+					
+					managedAccount = connectedAccounts.get(userInt - 1);
+					
+					if(!managedAccount.getIsApplication()) {
+						List<Account> validAccounts = getValidAccounts(connectedAccounts);
+						List<String> menuOptions = printTransactionMenu(validAccounts);
+						String transactionInput;
+						do {
+							transactionInput = scanner.nextLine().strip();
+							
+						}while(transactionInput.equals(""));
+						
+						for(String s : menuOptions) {
+							if (s.contains(transactionInput)) {
+								
+							}
+						}
+						
+					}
+					else {
+						System.out.println("This account is still pending approval.");
+					}
+				}
+				catch(NumberFormatException e) {
+					System.out.println("That is not a valid choice.");
+				}
+
+				
+				
+				
+			}
+		} while (!"cancel quit leave exit".contains(userInput));
 	}
-	
+
+
+	private List<String> printTransactionMenu(List<Account> validAccounts) {
+		List<String> menuOptions = new ArrayList<String>();
+		menuOptions.add("Deposit");
+		menuOptions.add("Withdraw");
+		if(validAccounts.size() >= 2)
+			menuOptions.add("Transfer");
+		menuOptions.add("Exit");
+		
+		for(int i = 0; i < menuOptions.size(); i++) {
+			menuOptions.set(i, "[" + (i+1) + "]   " + menuOptions.get(i));
+			System.out.println(menuOptions.get(i));
+		}
+		return menuOptions;
+	}
+
+
+	private List<Account> getValidAccounts(List<Account> accounts) {
+		List<Account> temp = new ArrayList<Account>();
+		
+		for(Account a : accounts) {
+			if (!a.getIsApplication()) {
+				temp.add(a);
+			}
+		}
+		
+		return temp;
+	}
 	
 	public void applyForAccount(Profile profile) {
 		
@@ -46,6 +112,8 @@ public class AccountMenu {
 			String userInput = "";
 			do {
 				userInput = scanner.nextLine().strip();
+				if (userInput.length() > 10)
+					userInput = "";
 			}while (userInput.equals(""));
 			
 			account.setAccountType(userInput);
@@ -57,6 +125,9 @@ public class AccountMenu {
 				userInput = scanner.nextLine().strip();
 			}while(userInput.equals(""));
 			
+			if (userInput.length() > 50)
+				userInput.subSequence(0, 10);
+			
 			account.setAccountName(userInput);
 			
 			validAccount = AccountServiceData.applyForAccount(account);
@@ -64,4 +135,82 @@ public class AccountMenu {
 		}while(!validAccount);
 		
 	}
+	
+	private List<Account> printAccounts(Profile profile) {
+		final int spacing = 5;
+		System.out.println("Here are all your accounts");
+		System.out.printf("%-"+spacing+"s %-"+ spacing*2 +"s %-" + spacing*3 +"s %s \n",
+				"#", "Balance" , "Account Type", "Account Name" );
+		System.out.println("====================================================");
+		
+		List<Account> userAccounts = AccountServiceData.getConnectedAccounts(profile);
+		
+		for(int i = 0; i < userAccounts.size(); i++){
+			Account acc = userAccounts.get(i);
+			String varString;
+			if(userAccounts.get(i).getIsApplication())
+				varString = "(PENDING)";
+			else
+				varString = userAccounts.get(i).getAccountType();
+			System.out.printf("%-"+spacing+"s %-"+ spacing*2 +"s %-" + spacing*3 +"s %s \n", (i+1) + ")", acc.getBalance(), varString, acc.getAccountName());
+		}
+		
+		if(userAccounts.size() <= 0) {
+			System.out.println("Sorry, we could not find any accounts under your details.\n"
+							+ "You can always apply for a bank account, and any approved bank accounts will appear here.");
+		}
+		
+		return userAccounts;
+	}
+	
+	private void deposit(Account account) {
+		System.out.println("How much would you like to deposit into your account?");
+		double funds = 0;
+		boolean validInput = false;
+		double newBalance;
+		do {
+			try {
+				funds = scanner.nextDouble();
+				
+				if(funds < 0)
+					throw new NumberFormatException("Input cannot be negative");
+				validInput = true;
+			}catch(NumberFormatException e) {
+				System.out.println("Invalid Input");
+			}
+		}while(!validInput);
+		AccountServiceData.getServiceData();
+		newBalance = AccountServiceData.depositFunds(account, funds);
+		if(newBalance == -1) {
+			System.out.println("Deposit did not complete.");
+		}else {
+			System.out.println("This account's new balance is " + newBalance);
+		}
+	}
+	
+	private void withdraw(Account account) {
+		System.out.println("How much would you like to withdraw from your account?");
+		double funds = 0;
+		boolean validInput = false;
+		double newBalance;
+		do {
+			try {
+				funds = scanner.nextDouble();
+				
+				if(funds < 0)
+					throw new NumberFormatException("Input cannot be negative");
+				validInput = true;
+			}catch(NumberFormatException e) {
+				System.out.println("Invalid Input");
+			}
+		}while(!validInput);
+		AccountServiceData.getServiceData();
+		newBalance = AccountServiceData.withdrawFunds(account, funds);
+		if(newBalance == -1) {
+			System.out.println("Withdraw did not complete.");
+		}else {
+			System.out.println("This account's new balance is " + newBalance);
+		}
+	}
+	
 }
